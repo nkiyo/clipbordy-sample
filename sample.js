7014b677f55f739658bca623d9c119e277e9b767
nkiyo <nakahara.kiyoshi+nkiyo@gmail.com>
2022-06-07 08:51:53 +0900

    クリップボードから読んだ結果をJSON化するようにした

--- sample.js
+++ sample.js
@@ -2,12 +2,14 @@
 // タブ区切りテキストのパース => https://stackoverflow.com/a/48670873
 // split関数の失敗チェック => https://stackoverflow.com/a/20578787
 import clipboard from 'clipboardy';
+import { get } from 'lodash-es';
 
 // クリップボードを読んでJSON化する
 // 以下、動作サンプル
 // クリップボードの内容：
 //   3	Vampire Weekend『Father of the Bride』
 //   5	小袋成彬『Piercing』
+//   XX	ほげ『ほげ/ほげ』
 // msgの内容：
 //   [{"id":"3","msg":"Vampire Weekend『Father of the Bride』"},{"id":"5","msg":"小袋成彬『Piercing』"}]
 //
@@ -16,7 +18,7 @@ import clipboard from 'clipboardy';
 //   => split結果の検証方法？
 const textFromClipboard = clipboard.readSync()
 console.log(`text from clipboard =>
-${textFromClipboard}`) // TODO よみづらい
+${textFromClipboard}`)
 const msgs = textFromClipboard
            .split('\r\n')
            .map(e => {
@@ -37,6 +39,20 @@ const msgs = textFromClipboard
 if(msgs.some(e => e === null)) {
   console.log(`parse failed`)
 } else {
-  console.log(`json =>
-${JSON.stringify(msgs)}`)
+  //console.log(`json =>
+//${JSON.stringify(msgs)}`)
 }
+
+const msgJson = {
+  MM: "xx",
+  YY: "xx",
+  msgs: [],
+  check: null,
+  jirei: null
+}
+msgJson.MM = "06"
+msgJson.YY = "07"
+msgJson.msgs = msgs.filter(e => parseInt(e.id)) // idが整数を意味する文字列の場合
+msgJson.check = get(msgs.find(e => !parseInt(e.id)), "msg") // idが整数以外を意味する文字列の場合
+console.log(JSON.stringify(msgJson))
+
